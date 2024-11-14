90f07f4080083a6dfb170c3a7aeab895632b9a0f
zeviko17 <cityjeruslaem@gmail.com>
2024-11-14 13:21:21 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    idInstance: '7103962196',
-    apiTokenInstance: '64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae',
-    sheetId: '10IkkOpeD_VoDpqMN23QFxGyuW0_p0TZx4NpWNcMN-Ss'
-};
