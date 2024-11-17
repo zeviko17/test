a2ed8cbec33933fa0e62511f60ab008d9c0acffb
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 13:12:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,7 @@ window.ENV_apiTokenInstance = '{{ .Env.NEXT_PUBLIC_apiTokenInstance }}';
 window.ENV_sheetId = '{{ .Env.NEXT_PUBLIC_sheetId }}';
 
 if (!window.ENV_idInstance || window.ENV_idInstance.includes('{{')) {
-    console.error('Environment variables not loaded from Vercel!');
+    window.ENV_idInstance = '7103962196';
+    window.ENV_apiTokenInstance = '64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae';
+    window.ENV_sheetId = '10IkkOpeD_VoDpqMN23QFxGyuW0_p0TZx4NpWNcMN-Ss';
 }
