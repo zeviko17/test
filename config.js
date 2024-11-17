9948fa28857570ce75fa5130515b89ea54b4a51e
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 11:53:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-
+window.ENV_idInstance = '7103962196';
+window.ENV_apiTokenInstance = '64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae';
+window.ENV_sheetId = '10IkkOpeD_VoDpqMN23QFxGyuW0_p0TZx4NpWNcMN-Ss';
