607062472bef41b2b2ab8890c44ed4d09862ebaf
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 12:00:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-const config = {
-    idInstance: process.env.NEXT_PUBLIC_idInstance,
-    apiTokenInstance: process.env.NEXT_PUBLIC_apiTokenInstance,
-    sheetId: process.env.NEXT_PUBLIC_sheetId
-};
+window.ENV_idInstance = process.env.NEXT_PUBLIC_idInstance;
+window.ENV_apiTokenInstance = process.env.NEXT_PUBLIC_apiTokenInstance;
+window.ENV_sheetId = process.env.NEXT_PUBLIC_sheetId;
 
-window.ENV_idInstance = config.idInstance;
-window.ENV_apiTokenInstance = config.apiTokenInstance;
-window.ENV_sheetId = config.sheetId;
+// נוסיף לוג לבדיקה
+console.log('Config values:', {
+    idInstance: window.ENV_idInstance,
+    apiTokenInstance: window.ENV_apiTokenInstance,
+    sheetId: window.ENV_sheetId
+});
