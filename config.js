9eafa5155d687b7d723989c6c8171003b81d4381
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 13:11:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 window.ENV_idInstance = '{{ .Env.NEXT_PUBLIC_idInstance }}';
 window.ENV_apiTokenInstance = '{{ .Env.NEXT_PUBLIC_apiTokenInstance }}';
 window.ENV_sheetId = '{{ .Env.NEXT_PUBLIC_sheetId }}';
+
+if (!window.ENV_idInstance || window.ENV_idInstance.includes('{{')) {
+    console.error('Environment variables not loaded from Vercel!');
+}
