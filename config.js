7cfefb6b32d2801af16eacf94f49a0d342dd843f
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 13:50:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,3 @@ window.config = {
     apiTokenInstance: '__NEXT_PUBLIC_apiTokenInstance__',
     sheetId: '__NEXT_PUBLIC_sheetId__'
 };
-
-// העברה למשתני ENV לתאימות עם הקוד הקיים
-window.ENV_idInstance = window.config.idInstance;
-window.ENV_apiTokenInstance = window.config.apiTokenInstance;
-window.ENV_sheetId = window.config.sheetId;
-
-// לוג לבדיקה
-console.log('Config loaded:', window.config);
