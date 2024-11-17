d8ea0b5f1632da4a1087ac492ccd5bb52c11d0f0
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 13:14:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,3 @@ window.ENV_idInstance = '{{ .Env.NEXT_PUBLIC_idInstance }}';
 window.ENV_apiTokenInstance = '{{ .Env.NEXT_PUBLIC_apiTokenInstance }}';
 window.ENV_sheetId = '{{ .Env.NEXT_PUBLIC_sheetId }}';
 
-if (!window.ENV_idInstance || window.ENV_idInstance.includes('{{')) {
-    window.ENV_idInstance = '7103962196';
-    window.ENV_apiTokenInstance = '64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae';
-    window.ENV_sheetId = '10IkkOpeD_VoDpqMN23QFxGyuW0_p0TZx4NpWNcMN-Ss';
-}
