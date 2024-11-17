da40d27194aa552bb7e8c5583fe8b57179cd68a8
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 11:58:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
-window.ENV_idInstance = '7103962196';
-window.ENV_apiTokenInstance = '64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae';
-window.ENV_sheetId = '10IkkOpeD_VoDpqMN23QFxGyuW0_p0TZx4NpWNcMN-Ss';
+const config = {
+    idInstance: process.env.NEXT_PUBLIC_idInstance || '7103962196',
+    apiTokenInstance: process.env.NEXT_PUBLIC_apiTokenInstance || '64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae',
+    sheetId: process.env.NEXT_PUBLIC_sheetId || '10IkkOpeD_VoDpqMN23QFxGyuW0_p0TZx4NpWNcMN-Ss'
+};
+
+// ייצוא הקונפיגורציה
+export default config;
