1d6eac658a161d8482f752c9fb622c273901d13d
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 12:01:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-window.ENV_idInstance = process.env.NEXT_PUBLIC_idInstance;
-window.ENV_apiTokenInstance = process.env.NEXT_PUBLIC_apiTokenInstance;
-window.ENV_sheetId = process.env.NEXT_PUBLIC_sheetId;
+// קריאת המשתנים מהסביבה
+window.ENV_idInstance = '__NEXT_PUBLIC_idInstance__';
+window.ENV_apiTokenInstance = '__NEXT_PUBLIC_apiTokenInstance__';
+window.ENV_sheetId = '__NEXT_PUBLIC_sheetId__';
 
-// נוסיף לוג לבדיקה
-console.log('Config values:', {
+// הוספת לוג לבדיקה
+console.log('Environment Variables:', {
     idInstance: window.ENV_idInstance,
     apiTokenInstance: window.ENV_apiTokenInstance,
     sheetId: window.ENV_sheetId
