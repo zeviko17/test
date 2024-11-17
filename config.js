6e16f5795f061e3db30024ccfb7ce571582c4887
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 12:06:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,8 @@
 window.ENV_idInstance = '{{ .Env.NEXT_PUBLIC_idInstance }}';
 window.ENV_apiTokenInstance = '{{ .Env.NEXT_PUBLIC_apiTokenInstance }}';
 window.ENV_sheetId = '{{ .Env.NEXT_PUBLIC_sheetId }}';
+window.ENV_accessCode = '{{ .Env.NEXT_PUBLIC_ACCESS_CODE }}';
 
-// נוסיף בדיקה והשמת ערכי ברירת מחדל אם המשתנים ריקים
 if (!window.ENV_idInstance || window.ENV_idInstance.includes('{{')) {
-    console.log('Using default values - VERCEL variables not loaded');
-    window.ENV_idInstance = '7103962196';
-    window.ENV_apiTokenInstance = '64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae';
-    window.ENV_sheetId = '10IkkOpeD_VoDpqMN23QFxGyuW0_p0TZx4NpWNcMN-Ss';
+    console.error('Environment variables not loaded from Vercel!');
 }
-
-console.log('Config loaded:', {
-    idInstance: window.ENV_idInstance,
-    apiTokenInstance: window.ENV_apiTokenInstance,
-    sheetId: window.ENV_sheetId
-});
