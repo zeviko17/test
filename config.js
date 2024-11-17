1ad160d05468e51402b33264c968928549d07197
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 12:02:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
-// קריאת המשתנים מהסביבה
-window.ENV_idInstance = '__NEXT_PUBLIC_idInstance__';
-window.ENV_apiTokenInstance = '__NEXT_PUBLIC_apiTokenInstance__';
-window.ENV_sheetId = '__NEXT_PUBLIC_sheetId__';
+window.ENV_idInstance = '{{ .Env.NEXT_PUBLIC_idInstance }}';
+window.ENV_apiTokenInstance = '{{ .Env.NEXT_PUBLIC_apiTokenInstance }}';
+window.ENV_sheetId = '{{ .Env.NEXT_PUBLIC_sheetId }}';
 
-// הוספת לוג לבדיקה
-console.log('Environment Variables:', {
+// נוסיף בדיקה והשמת ערכי ברירת מחדל אם המשתנים ריקים
+if (!window.ENV_idInstance || window.ENV_idInstance.includes('{{')) {
+    console.log('Using default values - VERCEL variables not loaded');
+    window.ENV_idInstance = '7103962196';
+    window.ENV_apiTokenInstance = '64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae';
+    window.ENV_sheetId = '10IkkOpeD_VoDpqMN23QFxGyuW0_p0TZx4NpWNcMN-Ss';
+}
+
+console.log('Config loaded:', {
     idInstance: window.ENV_idInstance,
     apiTokenInstance: window.ENV_apiTokenInstance,
     sheetId: window.ENV_sheetId
