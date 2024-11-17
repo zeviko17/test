fe5710ace5cde6c0519e80f784c812dfd653d20e
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 11:33:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const apiSendFileUrl = `https://7103.api.greenapi.com/waInstance${idInstance}/se
 
 // הגדרת גיליון
 const sheetId = window.ENV_sheetId;
+console.log('Sheet ID:', sheetId); // הוסף את זה
 const googleSheetsUrl = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:json&sheet=קבוצות%20להודעות`;
 
 // מערך לשמירת הקבוצות
@@ -22,6 +23,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 // טעינת קבוצות מהגיליון
 async function loadGroups() {
+    console.log('Google Sheets URL:', googleSheetsUrl); // הוסף את זה
     try {
         const response = await fetch(googleSheetsUrl);
         if (!response.ok) {
@@ -125,7 +127,7 @@ async function startSending() {
     }
 
     if (isProcessing) {
-        alert('תהליך שליחה כבר פועל');
+        alert('תהליך שליחה כבר פעל');
         return;
     }
 
