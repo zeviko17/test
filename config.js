5aa4ff37b39a427cb506e70a3c78e68f8485e409
zeviko17 <cityjeruslaem@gmail.com>
2024-11-17 13:20:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.ENV_idInstance = '{{ .Env.NEXT_PUBLIC_idInstance }}';
-window.ENV_apiTokenInstance = '{{ .Env.NEXT_PUBLIC_apiTokenInstance }}';
-window.ENV_sheetId = '{{ .Env.NEXT_PUBLIC_sheetId }}';
+window.ENV_idInstance = process.env.NEXT_PUBLIC_idInstance;
+window.ENV_apiTokenInstance = process.env.NEXT_PUBLIC_apiTokenInstance;
+window.ENV_sheetId = process.env.NEXT_PUBLIC_sheetId;
