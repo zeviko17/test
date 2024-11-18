ade47377ce1f63a1ea478df73bdc1c910b48988c
zeviko17 <cityjeruslaem@gmail.com>
2024-11-18 15:30:32 +0200

    Create config.sample.js

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+// config.sample.js
+window.ENV_idInstance = 'YOUR_ID_INSTANCE';
+window.ENV_apiTokenInstance = 'YOUR_API_TOKEN';
+window.ENV_sheetId = 'YOUR_SHEET_ID';
+
+console.log('Config values:', {
+    idInstance: window.ENV_idInstance,
+    apiTokenInstance: window.ENV_apiTokenInstance,
+    sheetId: window.ENV_sheetId
+});
